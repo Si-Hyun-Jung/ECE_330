047e1a4b6e7d27c8730d5196791f6c74f7d56c94
Si_Hyun_Jung <20191036@sungshin.ac.kr>
2021-03-10 15:14:17 -0700

    finished

--- Seleciton/Seleciton/Base.cpp
+++ Seleciton/Seleciton/Base.cpp
@@ -7,11 +7,15 @@ using namespace std;
 
 void selectionSort(int* const, const int); // prototype
 void swap(int* const, int* const); // prototype
+void selectionSort2(float* const, const int); // prototype
+void swap2(float* const, float* const); // prototype
 
 int main()
 {
     const int arraySize = 10;
     int a[arraySize] = { 2, 6, 4, 8, 10, 12, 89, 68, 45, 37 };
+    float b[arraySize] = { 2.2, 6.1, 4.5, 8.7, 10.6, 12.7, 89.1, 68.365, 45.15, 37.4 };
+
 
     cout << "Data items in original order\n";
 
@@ -26,7 +30,21 @@ int main()
         cout << setw(4) << a[j];
 
     cout << endl;
-} // end main
+
+cout << "Data items in original order\n";
+
+    for (int i = 0; i < arraySize; ++i)
+        cout << setw(7) << b[i];
+
+    selectionSort2(b, arraySize); // sort the array
+
+    cout << "\nData items in ascending order\n";
+
+    for (int j = 0; j < arraySize; ++j)
+        cout << setw(7) << b[j];
+
+    cout << endl;
+	} // end main
 
 // function to sort an array
 void selectionSort(int* const array, const int size)
@@ -48,11 +66,37 @@ void selectionSort(int* const array, const int size)
     } // end if
 } // end function selectionSort
 
+void selectionSort2(float* const array, const int size)
+{
+    int smallest; // index of smallest element
+
+    // loop over size - 1 elements
+    for (int i = 0; i < size - 1; ++i)
+    {
+        smallest = i; // first index of remaining array
+
+        // loop to find index of smallest element
+        for (int index = i + 1; index < size; ++index)
+
+            if (array[index] < array[smallest])
+                smallest = index;
+
+        swap2(&array[i], &array[smallest]);
+    } // end if
+} // end function selectionSort
+
 // swap values at memory locations to which
 // element1Ptr and element2Ptr point
 void swap(int* const element1Ptr, int* const element2Ptr)
 {
     int hold = *element1Ptr;
+    *element1Ptr = *element2Ptr;
+    *element2Ptr = hold;
+} // end function swap
+
+void swap2(float* const element1Ptr, float* const element2Ptr)
+{
+    float hold = *element1Ptr;
     *element1Ptr = *element2Ptr;
     *element2Ptr = hold;
 } // end function swap
